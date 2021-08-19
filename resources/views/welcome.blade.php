d0ee630c3af54e7c10d0c3fb854a9718643406f2
Tran Xuan Huong <phamhyta@gmail.com>
2021-08-19 12:52:03 +0700

    --- Auto-Git Commit ---

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,12 +8,25 @@
     <title>Document</title>
 </head>
 <body class="bg-white">
-    <div class="w-full h-14 border-2">
+    <div class="w-full h-28 border-2">
+        menu
     </div>
     <div class="w-full border-2 h-96">
+        san pham
     </div>
-    <div class="absolute left-0 top-0 w-1/2 boder-2 h-14">
-
+    <div class="relative w-full border-2 h-96">
+        <div class="absolute left-0 top-0 w-2/5 border-2 h-48">
+            hinh anh
+        </div>
+        <div class="absolute bottom-0 right-0 w-2/5 border-2 h-48">
+            danh gia
+        </div>
+    </div>
+    <div class="w-full h-28 border-2">
+        <div class="w-1/4 h-24 p-2px">1</div>
+        <div class="w-1/4 h-24 p-2px">2</div>
+        <div class="w-1/4 h-24 p-2px">3</div>
+        <div class="w-1/4 h-24 p-2px">4</div>
     </div>
     @yield('content') 
 </body>
