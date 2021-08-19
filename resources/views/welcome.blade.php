0824cc9eaab3505ab001cdc9878197f63d0ef927
Tran Xuan Huong <phamhyta@gmail.com>
2021-08-19 11:54:12 +0700

    --- Auto-Git Commit ---

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,18 +7,13 @@
     <link rel="stylesheet" href="../css/app.css">
     <title>Document</title>
 </head>
-<body class="bg-gray-700">
-    <nav class="bg-gray-900 flex justify-between text-white">
-        <ul class="flex p-2">
-            <li class="px-2"><a href="">Home</a></li>
-            <li class="px-2"><a href="">Student</a></li>
-            <li class="px-2"><a href="">Class</a></li>
-        </ul>
-        <ul class="flex p-2">
-            <li class="px-2"><a href="">Sing in</a></li>
-            <li class="px-2"><a href="">Logout</a></li>
-        </ul>
-    </nav>
+<body>
+    <div class="w-full h-1/4 bg-gray-100 border-2">
+        abc
+    </div>
+    </div class="w-full h-1066 bg-gray-100 mt-2 ">
+        abc
+    <div>
     @yield('content') 
 </body>
 </html>
