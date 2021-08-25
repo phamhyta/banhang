aeb35a8840de6eb3db14e43146e7729c9987bf62
Lê Minh Tiến <minhtienlomo@gmail.com>
2021-08-25 22:06:01 +0700

    Add files via upload

---
+++ resources/views/testGrid.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body class=" bg-blue-400 flex items-center justify-center min-h-screen">
+    <div class="w-max grid grid-cols-3 gap-2">
+        <div class="bg-white p-20 rounded row-span-2">
+            1
+            {{-- <img src="{{asset("img/nav_shoes_color(1).png")}}"> --}}
+        </div>
+        <div class="bg-white p-20 rounded">
+            2
+        </div>
+        <div class="bg-white p-20 rounded row-span-2">
+            3
+            {{-- <img src="{{asset("img/nav_shoes_color(4).png")}}"> --}}
+        </div>
+        <div class="bg-white p-20 rounded row-span-2">
+            4
+            {{-- <img src="{{asset("img/nav_shoes_color(2).png")}}"> --}}
+        </div>
+        <div class="bg-white p-20 rounded">
+            5
+            {{-- <img src="{{asset("img/nav_shoes_color(3).png")}}"> --}}
+        </div>
+        <div class="bg-white p-20 rounded">
+            6
+            {{-- <img src="{{asset("img/nav_shoes_color(5).png")}}"> --}}
+        </div>
+    </div>
+</body>
+</html>
